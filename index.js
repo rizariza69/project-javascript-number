487a5e18ee26623e53b4f1d213d5b69765579919
Muhammad Riza <riza.riza69@gmail.com>
2019-04-04 17:37:41 +0700

    improve index.js

--- index.js
+++ index.js
@@ -10,3 +10,19 @@ console.log(`His age is ${age}`)
 
 alert(`His age is ${age}`)
 
+let wordCount = 1;
+
+if (wordCount) {
+  console.log("Great! You've started your work!");
+} else {
+  console.log('Better get to work!');
+}
+
+let number = 5;
+let compareNumber = 20;
+
+console.log(20 + 5);
+console.log(20 * 5);
+
+console.log(`${number} / ${compareNumber}`)
+console.log(number / compareNumber)
